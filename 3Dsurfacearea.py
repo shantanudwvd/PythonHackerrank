9d7b0088cda0db47f2491714ec9978f948f34ab6
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-02-02 16:51:19 +0530

    Final Commit

--- 3Dsurfacearea.py
+++ 3Dsurfacearea.py
@@ -1,40 +1,39 @@
-def surfaceArea(A):
+def surfaceArea(array):
     top_bottom = 2 * W * H
-    total_area = 0
     area = 0
     if H == 1:
         for i in range(0, W):
-            area += A[0][i]
+            area += array[0][i]
         area *= 2
         for i in range(1, W):
-            area += abs(A[0][i] - A[0][i - 1])
-        area += A[0][0]
-        area += A[0][W - 1]
+            area += abs(array[0][i] - array[0][i - 1])
+        area += array[0][0]
+        area += array[0][W - 1]
         area += top_bottom
         total_area = area
     elif W == 1:
         for i in range(0, H):
-            area += A[i][0]
+            area += array[i][0]
         area *= 2
         for i in range(1, H):
-            area += abs(A[i][0] - A[i - 1][0])
-        area += A[0][0]
-        area += A[H - 1][0]
+            area += abs(array[i][0] - array[i - 1][0])
+        area += array[0][0]
+        area += array[H - 1][0]
         area += top_bottom
         total_area = area
     else:
         for i in range(0, H):
-            area += A[i][0]
-            area += A[i][W - 1]
+            area += array[i][0]
+            area += array[i][W - 1]
         for i in range(0, W):
-            area += A[1][i]
-            area += A[H - 1][i]
+            area += array[1][i]
+            area += array[H - 1][i]
         for i in range(0, W):
             for j in range(1, H):
-                area += abs(A[j][i] - A[j - 1][i])
+                area += abs(array[j][i] - array[j - 1][i])
         for i in range(0, H):
             for j in range(1, W):
-                area += abs(A[i][j] - A[i][j - 1])
+                area += abs(array[i][j] - array[i][j - 1])
         total_area = area + top_bottom
     return total_area
 
