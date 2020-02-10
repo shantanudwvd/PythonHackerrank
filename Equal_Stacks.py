485473d3898c5fd62e02ffa2e3d39d6b587f6238
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-02-10 18:45:27 +0530

    Completed Equal Stacks problem

---
+++ Equal_Stacks.py
@@ -0,0 +1,48 @@
+s1, s2, s3 = map(int, input().split(" "))
+a1 = list(map(int, input().split()))
+a2 = list(map(int, input().split()))
+a3 = list(map(int, input().split()))
+sum1 = sum(a1)
+sum2 = sum(a2)
+sum3 = sum(a3)
+i1 = 0
+i2 = 0
+i3 = 0
+while True:
+    if sum1 == sum2 and sum2 == sum3:
+        print(sum1)
+        break
+    elif sum1 == 0 or sum2 == 0 or sum3 == 0:
+        print("0")
+        break
+    elif sum1 > sum3 and sum2 > sum3:
+        sum1 -= a1[i1]
+        i1 += 1
+        sum2 -= a2[i2]
+        i2 += 1
+    elif sum2 > sum1 and sum3 > sum1:
+        sum2 -= a2[i2]
+        i2 += 1
+        sum3 -= a3[i3]
+        i3 += 1
+    elif sum1 > sum2 and sum3 > sum2:
+        sum3 -= a3[i3]
+        i3 += 1
+        sum1 -= a1[i1]
+        i1 += 1
+    elif sum1 > sum2 and sum1 > sum3:
+        sum1 -= a1[i1]
+        i1 += 1
+    elif sum2 > sum1 and sum2 > sum3:
+        sum2 -= a2[i2]
+        i2 += 1
+    elif sum3 > sum1 and sum3 > sum2:
+        sum3 -= a3[i3]
+        i3 += 1
+    else:
+        sum1 -= a1[i1]
+        i1 += 1
+        sum2 -= a2[i2]
+        i2 += 1
+        sum3 -= a3[i3]
+        i3 += 1
