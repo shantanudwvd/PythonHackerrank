0c4623a0417e306829d9456ebb318accb0656749
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-10-18 20:41:24 +0530

    Updated .idea/workspace.xml files

--- TwoCharacters.py
+++ TwoCharacters.py
@@ -2,6 +2,4 @@ from collections import Counter
 size = int(input())
 string = input()
 myDict = dict(Counter(string))
-for k, v in myDict.items():
-    print(f"key is: {k} and value is: {v}")
 print(myDict)
